bdaf3e9572715e6387e833b2963e3fb05aa7b262
Ganga T Dharan <gangatdharan16@gmail.com>
2022-10-12 12:30:15 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django import http
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello world')
